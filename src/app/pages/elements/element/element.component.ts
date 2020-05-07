5e66e9ef9f5c08d3ac74aad954e3df1d0a4dfe12
Olivier Pujadas <pujadas.olivier@gmail.com>
2020-05-07 14:41:11 +0200

    Correction bug edition élément : sur annulation, ça annule !

--- src/app/pages/elements/element/element.component.ts
+++ src/app/pages/elements/element/element.component.ts
@@ -1,30 +1,20 @@
-import { Component, Input, OnDestroy, Inject } from '@angular/core';
-
-import { Element } from './../../../core/model/element/element.model';
-import { ElementService } from './../../../core/service/element.service';
-
+// Core components
+import { Component, Input, OnInit, OnDestroy, Inject } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormsModule, ReactiveFormsModule, Validators, FormGroup, FormControl } from '@angular/forms'; 
 
-import { FormGroup, FormControl } from '@angular/forms'; 
-import { FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-
-/*import { TagListService } from './../../../core/service/tag-list.service';
-
-import { TagService } from './../../../core/service/tag.service';
-import { MessageSocket } from './../../../core/model/message-socket/message-socket.model';
-
-import { Router, ActivatedRoute } from '@angular/router';
-
-import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
-import { Socket } from 'ng-socket-io';
-*/
-import { HtmlcontentPipe }          from '../../../core/pipes/htmlcontent.pipe';
-
-import { ToastsManager } from 'ng6-toastr/ng2-toastr';
+// Plugins & modules 
 import { TranslateService } from 'ng2-translate';
+import { ToastsManager } from 'ng6-toastr/ng2-toastr';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+// Custom models
+import { Element } from './../../../core/model/element/element.model';
+
+// Custom services
+import { ElementService } from './../../../core/service/element.service';
+import { HtmlcontentPipe }          from '../../../core/pipes/htmlcontent.pipe';
 
 
 
@@ -34,20 +24,31 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   styleUrls: ['./element.component.css']
 })
 
-export class ElementComponent  implements OnDestroy {
+export class ElementComponent  implements OnInit, OnDestroy {
+
+  // Custom variables & attributes
+
   @Input()
   element : Element; 
 
-  private subscriptionAddTag: Subscription;
-  private subscriptionDeleteTag : Subscription; 
-  private subscriptionUpdate : Subscription; 
+  private elementPreEdition : Element; 
+
+  // Subscriptions (don't forget to unsubscribe !)
+
+  private subscriptionAddTag :      Subscription;
+  private subscriptionDeleteTag :   Subscription; 
+  private subscriptionUpdate :      Subscription; 
 
   constructor(
     private _elementService : ElementService, 
     public dialog: MatDialog,
     public toastr: ToastsManager, 
-    private _translate: TranslateService, 
+    private _translate: TranslateService 
     )  {     
+  }
+
+  ngOnInit(){
+
   }
 
   /* Fonction deleteTag(event) 
@@ -57,29 +58,18 @@ export class ElementComponent  implements OnDestroy {
 
   deleteTag(event){
 
-
-      console.log(this.element.tags);
-      console.log('event');
-      console.log(event);
-
       let index = (this.element.tags).findIndex(x => x._id == event); 
-      console.log('INDEX : ' + index); 
-      if (index == -1){
-        console.log('chaine non trouvée');
-
+      if (index == -1){ // chaine non trouvée
         // On envoie un message à l'utilisateur
         this._translate.get('TOASTER.TAG.DELETE_FROM_ELEMENT.WARNING').subscribe((res: string) => {
             console.log(res);
             this.toastr.success(res, 'Warning !');
         });                                                     
-      } else {
-         console.log('index trouvé : ');
-
+      } else { // chaine trouvée
          // On supprime le tag de la BDD
-         this.subscriptionDeleteTag = this._elementService.deleteTagFromElement(event, this.element._id)
-                                        .subscribe(data => {
-                                            console.log(data); 
-                                        }); 
+         this.subscriptionDeleteTag = this._elementService
+                                        .deleteTagFromElement(event, this.element._id)
+                                        .subscribe(data => { console.log(data); }); 
          // On supprime visuellement le tag
          this.element.tags.splice(index , 1); 
 
@@ -92,9 +82,12 @@ export class ElementComponent  implements OnDestroy {
 
   editElement(element) : void {
 
+    // On clone this.view dans backup : 
+    this.elementPreEdition = JSON.parse(JSON.stringify(this.element)); 
+    console.log('INIT element avant edition : '); 
+    console.log(this.elementPreEdition); 
     console.log('On ouvre une fenetre dialogue pour affichier / editer element'); 
     console.log(element); 
-
  
     this.element = element;
         
@@ -105,22 +98,33 @@ export class ElementComponent  implements OnDestroy {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('ici The dialog was closed');
-      // this.refreshList(); 
-    // On met à jour la BDD pour associer le event.dragData au post
-    this.subscriptionUpdate = this._elementService.updateElement(this.element)
-                            .subscribe(data => {
-                                console.log(data);
-                                // this.dialogRef.close();
-/*
+      console.log(result); 
+
+      // Si on a validé une mise à jour, on a un result
+      if (result !== undefined){
+        // On doit raffraichir les vues root, on informe le viewService
+        //this.subscriptionViewChanged = this._viewService.sendViewAction(this.view);             
+        // On met à jour la BDD pour associer le event.dragData au post
+        this.subscriptionUpdate = this._elementService.updateElement(this.element)
+                                .subscribe(data => {
+                                    console.log(data); });
         // On toaste pour l'utilisateur en cours
         this._translate.get('TOASTER.ELEMENT.UPDATE.SUCCESS').subscribe((res: string) => {
             console.log(res);
-            this.toastr.success(res, 'Success!');
+            this.toastr.success(res, 'Success !');
         });                                  
-  */    
-      });       
-    });
+      } 
+      // Si on a annulé l'édition, le result est undefined
+      else {
+          this.element = JSON.parse(JSON.stringify(this.elementPreEdition));
+
+          // On toaste pour l'utilisateur en cours
+          this._translate.get('TOASTER.ELEMENT.UPDATE.CANCEL').subscribe((res: string) => {
+              this.toastr.info(res, 'Information');
+          });                                            
+      }
      
+  });
   }
 
 
@@ -202,13 +206,6 @@ export class DialogEditPost implements OnDestroy  {
     private _translate: TranslateService, 
     @Inject(MAT_DIALOG_DATA) public element: Element) { 
 
-    // if (element.data.title) this.oldtitle = element.data.title; 
-    // if (element.data.content) this.oldcontent = element.data.content; 
-
-    // On injecte le titre du post pour pouvoir l'inclure dans la boite de dialogue et pouvoir faire la traduction
-    // this.element = element;
-    console.log('Element depuis le dialogue :'); 
-    console.log(this.element); 
     console.log('Dialog constructeur'); 
 
     this.editPostForm = new FormGroup({
@@ -218,35 +215,20 @@ export class DialogEditPost implements OnDestroy  {
   }
 
 
+
   onSubmit(){
-    console.log('Submit'); 
-    console.log(this.element);
-    console.log('Elément updaté ?'); 
-    console.log(this.editPostForm); 
-
     // On met à jour la BDD pour associer le event.dragData au post
-    this.subscriptionUpdate = this._elementService.updateElement(this.element)
-                            .subscribe(data => {
-                                console.log(data);
-                                this.dialogRef.close();
+    this.subscriptionUpdate = this._elementService
+            .updateElement(this.element)
+            .subscribe(data => {
+                // Ici on passe la data en paramètre pour différencier
+                // une action avec mise à jour (comme ici), d'une action annulée
+                this.dialogRef.close(data); 
 
-        // On toaste pour l'utilisateur en cours
-        this._translate.get('TOASTER.ELEMENT.UPDATE.SUCCESS').subscribe((res: string) => {
-            console.log(res);
-            this.toastr.success(res, 'Success!');
-        });                                  
-                            }); 
-       
+     });  
   }
 
   onNoClick(): void {
-    console.log('on no click'); 
-
-    // On réassigne les anciennes valeurs
-    // this.element.data.title = this.oldtitle; 
-    // this.element.data.content = this.oldcontent; 
-    
-
     this.dialogRef.close();
   }
 
